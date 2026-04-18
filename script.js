fcdc33728077ce891b6b5e99f94cb20a56d874ef
Gurmeet Kaur <gkdev99@gmail.com>
2026-04-18 12:27:16 +0530

    update ui

--- script.js
+++ script.js
@@ -52,3 +52,9 @@ topBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
